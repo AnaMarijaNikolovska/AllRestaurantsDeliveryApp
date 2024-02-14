67603f25b17db2e66819d25ede029bfa3bcd326c
AnaMarijaNikolovska <ana.nikolovska13@yahoo.com>
2024-02-14 10:49:08 +0100

    home page

--- frontend/src/routes/root.jsx
+++ frontend/src/routes/root.jsx
@@ -1,19 +1,13 @@
 import {useLoaderData} from "react-router-dom";
 import {Grid} from "@mui/material";
 import RestoranCard from "../components/cards/restoran-card";
+import LoginPhoto from "../assets/images/delivery.jpg";
+import HomePage from "../assets/images/AllRestaurantsHomePage.jpg"
 
 export default function Root() {
-    const loaderData = useLoaderData();
 
     return (
-        <Grid container>
-            {loaderData.restorants &&
-                loaderData.restorants.length > 0 &&
-                loaderData.restorants.map((restoran) => (
-                    <Grid key={restoran.id} item xs={12} md={6} lg={4}>
-                        <RestoranCard restoran={restoran}/>
-                    </Grid>
-                ))}
+        <Grid container component={"main"} style={{backgroundImage: `url(${HomePage})`}} className={"background-image"}>
         </Grid>
     );
 }
